ef8beb32eb8bd2e3af68ce6ba75218b4e08b1153
Aabhas <aabhassao0@gmail.com>
2021-03-19 23:35:49 +0530

    firebase auth google sign in

---
+++ lib/screens/ui/error.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SomethingWentWrong extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        child: Text('something went wrong'),
+      ),
+    );
+  }
+}
